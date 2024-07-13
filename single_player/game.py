801f59c47e8fe90f135dae1d7a93e4986368bc3e
Flytrap123 <jackiecheng2306@gmail.com>
2024-07-13 13:50:23 +1000

    hitbox and stuff

--- single_player/game.py
+++ single_player/game.py
@@ -6,6 +6,10 @@ import random
 # these should be moved to constants
 WIDTH = 750
 HEIGHT = 1000
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 
 class Ship():
@@ -30,11 +34,25 @@ class Ship():
         self._vel = 5
         self._bullets = []
 
+        self.ship_img = YELLOW_SPACE_SHIP   # placeholder
+        self.hitbox = pygame.mask.from_surface(self.ship_img)
+    
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
+
+    def add_x(self, value: int):
+        self._x += value
+
+    def add_y(self, value: int):
+        self._y += value
+
 
 class Player(Ship):
     def draw(self, win) -> None:
-        # draw a rectangle (the player)
-        pygame.draw.rect(win, self._colour, self._rect)
+        WIN.blit(self.ship_img, (self.get_x(), self.get_y()))
 
     def move(self) -> None:
         # add key listener
@@ -85,7 +103,7 @@ class Enemy(Ship):
     def draw(self, win):
         """Draw the enemy"""
         self.move()
-        pygame.draw.circle(win, self._colour, (self._x, self._y), 5)
+        WIN.blit(self.ship_img, (self.get_x(), self.get_y()))
 
     def move(self):
         """Move the enemy downwards"""
@@ -157,6 +175,15 @@ def redrawWindow(win, player: Player, enemies: list[int], bullets: list[int]):
     # update window
     pygame.display.update()
 
+'''
+ent1 = one of the entities involved in the collision
+ent2 = the other entity
+'''
+def has_collided(ent1, ent2):
+    offset_x = ent2.get_x() - ent1.get_x()
+    offset_y = ent2.get_y() - ent1.get_y()
+    return ent1.hitbox.overlap(ent2.hitbox, (offset_x, offset_y)) != None
+    # returns True if the entities touch, otherwise returns False
 
 def main():
     # Variable to keep our game loop running
@@ -219,6 +246,11 @@ def main():
             else:
                 # out of bounds
                 bullets.pop(bullets.index(bullet))
+
+        for enemy in enemies:
+            if has_collided(enemy, p1):
+                p1._health -= 10
+                enemies.remove(enemy)
 
         # redraw window
         redrawWindow(win, p1, enemies, bullets)
