27f885406d05cc699c7e0eb619606b92da268d97
Alice-Lesmes <alesmes3579@gmail.com>
2024-07-18 13:10:07 +1000

    Forgor to commit android studio files again. Eto...Bleh

---
+++ Android/Phone_Client/settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://github.com/irmen/pickle")
+/*
+        maven { url'https://github.com/irmen/pickle' }
+*/
+    }
+}
+
+rootProject.name = "AsymetricAsteroids"
+include(":app")
+ 
